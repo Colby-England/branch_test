0a65b271b026cd52b1936b0aa87c2862c0a9aaef
Colby England <colby.england@gmail.com>
2022-05-16 21:20:17 -0500

    broken

--- person.py
+++ person.py
@@ -4,10 +4,7 @@ class Person:
 
     def __init__(self, name, age):
         self.name = name
-        self.age = age
 
     def get_name(self):
         return self.name
 
-    def get_age(self):
-        return self.age
